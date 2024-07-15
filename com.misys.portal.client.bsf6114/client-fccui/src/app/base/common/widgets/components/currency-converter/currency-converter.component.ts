968adec8eaa9d3d60bd8235cd641727c4ad4dd15
FatimaBaherIBC <fatima.b@ibcconsult.com>
2024-07-15 20:59:27 +0300

    FIN - 999
    I did some changes...

--- com.misys.portal.client.bsf6114/client-fccui/src/app/base/common/widgets/components/currency-converter/currency-converter.component.ts
+++ com.misys.portal.client.bsf6114/client-fccui/src/app/base/common/widgets/components/currency-converter/currency-converter.component.ts
@@ -17,6 +17,7 @@ export class CurrencyConverterComponent implements OnInit {
   selectedCurrency;
   calculatedAmount: number;
   //This is change number 1
+  //changes
   fromCurrency: any;
   toCurrency: any ;
   fromAmount = '100';
